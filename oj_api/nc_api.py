61f5ccc4d10e7c5523cce835a77a23bfd2789224
INGg <1095490883@qq.com>
2022-02-24 23:15:52 +0800

    修复了牛客无法跨月获取比赛

--- oj_api/nc_api.py
+++ oj_api/nc_api.py
@@ -4,10 +4,13 @@ import time
 import datetime
 import asyncio
 import pprint
+from dateutil.relativedelta import relativedelta
 
 
 async def get_contest():
-    date = str(datetime.datetime.now().year) + '-' + str(datetime.datetime.now().month)
+    now = datetime.datetime.now()
+
+    date = str(now.year) + '-' + str(now.month)
     url = "https://ac.nowcoder.com/acm/calendar/contest?token=&month=" + date + "&_=" + str(int(time.time()) * 1000)
 
     json_data = await get_json(url)
@@ -19,38 +22,40 @@ async def get_contest():
 
     lately_contest = dict()
 
-    try_time = 0
+    for contest in contest_data:
+        if contest['ojName'] == 'NowCoder' \
+                and contest['startTime'] >= int(time.time()) * 1000 \
+                and ("专题" not in contest['contestName']):
+            lately_contest = contest
+            break  # 只获取马上要举行的牛客比赛
 
-    while try_time < 5:  # 重试五次，每次间隔20分钟
-        for contest in contest_data:
-            if contest['ojName'] == 'NowCoder' \
-                    and contest['startTime'] >= int(time.time()) * 1000 \
-                    and ("专题" not in contest['contestName']):
-                lately_contest = contest
-                break  # 只获取马上要举行的牛客比赛
+    if lately_contest.__contains__('endTime') and lately_contest.__contains__('startTime'):
+        durationSeconds = (int(lately_contest['endTime']) - int(lately_contest['startTime'])) // 1000
+    else:
+        now += relativedelta(months=1)
+        date = str(now.year) + '-' + str(now.month)
+        url = "https://ac.nowcoder.com/acm/calendar/contest?token=&month=" + date + "&_=" + str(
+            int(time.time()) * 1000)
 
-        if lately_contest.__contains__('endTime') and lately_contest.__contains__('startTime'):
-            durationSeconds = (int(lately_contest['endTime']) - int(lately_contest['startTime'])) // 1000
-        else:
-            try_time += 1
+        json_data = await get_json(url)
 
-            print("获取牛客失败，正在重试")
-            await asyncio.sleep(20 * 60)  # 休息20分钟再试
-            # pprint.pprint(lately_contest)
-            continue
+        if json_data['code'] != 0:
+            return -1
 
-        res = "下一场牛客比赛为：\n" \
-              "比赛名称：{}\n" \
-              "开始时间：{}\n" \
-              "持续时间：{}\n" \
-              "比赛地址：{}".format(
-            lately_contest['contestName'],
-            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(lately_contest['startTime']) // 1000)),
-            "{}小时{:02d}分钟".format(durationSeconds // 3600, durationSeconds % 3600 // 60),
-            lately_contest['link']
-        )
+        contest_data += json_data['data']
 
-        return res, int(lately_contest['startTime'] // 1000)
+    res = "下一场牛客比赛为：\n" \
+          "比赛名称：{}\n" \
+          "开始时间：{}\n" \
+          "持续时间：{}\n" \
+          "比赛地址：{}".format(
+        lately_contest['contestName'],
+        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(lately_contest['startTime']) // 1000)),
+        "{}小时{:02d}分钟".format(durationSeconds // 3600, durationSeconds % 3600 // 60),
+        lately_contest['link']
+    )
+
+    return res, int(lately_contest['startTime'] // 1000)
 
 
 
