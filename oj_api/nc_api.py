6c494951df1b40c00fd87151f44ff0b13700094d
INGg <1095490883@qq.com>
2022-02-25 10:34:30 +0800

    加快了牛客比赛的获取速度，精简了代码

--- oj_api/nc_api.py
+++ oj_api/nc_api.py
@@ -8,36 +8,33 @@ from dateutil.relativedelta import relativedelta
 
 
 async def get_contest():
-    now = datetime.datetime.now()
+    async def find():
+        now = datetime.datetime.now()
 
-    date = str(now.year) + '-' + str(now.month)
-    url = "https://ac.nowcoder.com/acm/calendar/contest?token=&month=" + date + "&_=" + str(int(time.time()) * 1000)
+        try:
+            for _ in range(12):
+                now = now + relativedelta(months=1)
+                date = str(now.year) + '-' + str(now.month)
+                url = "https://ac.nowcoder.com/acm/calendar/contest?token=&month=" + date + "&_=" + str(int(time.time()) * 1000)
 
-    json_data = await get_json(url)
+                json_data = await get_json(url)
 
-    if json_data['code'] != 0:
-        return -1
+                contest_data = json_data['data']
 
-    contest_data = json_data['data']
+                for contest in contest_data:
+                    if contest['ojName'] == 'NowCoder' \
+                            and contest['startTime'] >= int(time.time()) * 1000 \
+                            and ("专题" not in contest['contestName']):
+                        lately_contest = contest
+                        if lately_contest.__contains__('endTime') and lately_contest.__contains__('startTime'):
+                            durationSeconds = (int(lately_contest['endTime']) - int(lately_contest['startTime'])) // 1000
+                            return lately_contest, durationSeconds  # 只获取马上要举行的牛客比赛
+        except:
+            return -1
 
-    next = now + relativedelta(months=1)
-    next_date = str(next.year) + '-' + str(next.month)
-    next_url = "https://ac.nowcoder.com/acm/calendar/contest?token=&month=" + next_date + "&_=" + str(
-        int(time.time()) * 1000)
-    json_next_data = await get_json(next_url)
-    
-    if json_next_data['code'] == 0:
-        contest_data += json_next_data['data']
+    lately_contest, durationSeconds = await find()
 
-    for contest in contest_data:
-        if contest['ojName'] == 'NowCoder' \
-                and contest['startTime'] >= int(time.time()) * 1000 \
-                and ("专题" not in contest['contestName']):
-            lately_contest = contest
-            if lately_contest.__contains__('endTime') and lately_contest.__contains__('startTime'):
-                durationSeconds = (int(lately_contest['endTime']) - int(lately_contest['startTime'])) // 1000
-                break  # 只获取马上要举行的牛客比赛
-    else:
+    if lately_contest == -1:
         return -1
 
     res = "下一场牛客比赛为：\n" \
